689bd530ad734ed0dd649b8634032e79352ca4e3
Ungguh Faizaturrohman <otemoos@outlook.com>
2021-07-24 09:12:13 +0700

    express sampai validasi dan webhooks

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const PORT = process.env.PORT || 9000
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// port listen
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`)
+})
+
+// database connection
+const db = require('./config/connection')
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => {
+        console.log('Database connected!')
+    }).catch((err) => {
+        console.log("Cannot connect to the database!", err)
+        process.exit()
+    })
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes)
