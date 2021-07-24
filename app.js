dc368bbf5d5e47bc6f25932ae055da098fb9c43a
Ungguh Faizaturrohman <otemoos@outlook.com>
2021-07-24 10:56:27 +0700

    preparation cookie

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const PORT = process.env.PORT || 9000
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
 
 // port listen
 app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`)
+    console.log(`server is running on http://localhost:${PORT}`)
 })
 
 // database connection
@@ -35,3 +37,28 @@ db.mongoose
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+
+    // res.setHeader('Set-Cookie', 'newUser=true');
+
+    res.cookie('newUser', false);
+    res.cookie('isEmployee', true, {
+        // 1000ms * 60s * 60 menit * 24 jam
+        maxAge: 1000 * 60 * 60 * 24,
+        httpOnly: true
+        // output: document.cookie => value cookie yg di atas 
+        // secure: true --if using https
+        // httpOnly: true --if using http
+    });
+
+    res.send('yout got the cookie!');
+});
+
+app.get('/read-cookies', (req, res) => {
+    const cookies = req.cookies;
+    console.log(cookies.newUser);
+
+    res.json(cookies);
+});
